2049f563c406e96c678385c20aa865ef12951385
BK <rlaqudrnr810@naver.com>
2020-10-27 18:40:59 +0900

    chore: webpack babel 설치 및 설정
    babel 필수 모듈, core-js 모듈 설치
    babel.config 파일 추가
    webpack, webpack-cli, babel-loader, css-loader, style-loader, html
    webpack plugin, webpack-dev-server 설치
    webpack.config 파일 추가

---
+++ frontend/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+              test: /\.(js|jsx)$/,
+              use: 'babel-loader',
+              exclude: /node_modules/
+            },
+            {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+          template: 'src/index.html'
+        }
+    )],
+    devServer: {
+      contentBase: path.join(__dirname, './dist'),
+      compress: true,
+      overlay: true,
+      stats: 'errors-only',
+      port: 8081,
+      hot: true,
+      proxy: {
+        '/api': 'http://127.0.0.1:3000',
+      },
+    },
+  
+}
