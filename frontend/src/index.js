2e7f2abec46c5f445ff2a68e09e9309815c1bebe
kimseoyoung <caribou503@gmail.com>
2020-10-29 10:47:19 +0900

    update: 프론트엔드 디렉토리 구조 설계
    리액트 container 패턴을 적용
    src/components : 공통 사용 컴포넌트 모음
    src/containers : 데이터 처리를 담당하는 컨테이너 모음
    src/containers/components : 특정 컨테이너에 사용되는 커스텀 컴포넌트
    src/App.js : 루트 컴포넌트

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+import App from './App';
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App></App>,
   document.getElementById('root')
 );
