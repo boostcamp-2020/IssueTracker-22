5484288da7307a622753f8e9791b641188eb6cc5
YimJiYoung <chiy323@gmail.com>
2020-11-02 15:02:01 +0900

    update: global style 적용하기 위한 컴포넌트 정의
    - styled-component의 createGlobalStyle 함수를 이용하여 기본 css 속성을 없애고
    global하게 스타일을 정의할 수 있도록 assets/styles에 GlobalStyle 컴포넌트 정의

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
+// import Header from './components/Header';
+import GlobalStyle from './assets/styles/GlobalStyle';
 import LoginContainer from './containers/login/LoginContainer';
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <GlobalStyle bg="#f6f8fa"/>
       <LoginContainer />
     </>
   );
