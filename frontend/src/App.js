9c0294468051ee3fcd66293d52bf1b7ebe11b9a8
YimJiYoung <chiy323@gmail.com>
2020-11-03 21:18:18 +0900

    feat: react-router-dom 모듈 설치, router 적용
    - react-router-dom: react에서 routing을 구현할 수 있도록 해주는 라이브러리
    - routes.js : Route 설정 파일 (path와 해당 path에 렌더할 component 정의)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
 // import Header from './components/Header';
 import GlobalStyle from './assets/styles/GlobalStyle';
-import LoginContainer from './containers/login/LoginContainer';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router>
       {/* <Header /> */}
       <GlobalStyle bg="#f6f8fa"/>
-      <LoginContainer />
-    </>
+      <Routes />
+    </Router>
   );
 }
 
