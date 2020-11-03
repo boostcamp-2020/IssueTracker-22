28ce8ac4f0ab18ffe9e01b91eeeca4fb3c239a3d
kimseoyoung <caribou503@gmail.com>
2020-11-03 10:59:43 +0900

    feat: jwt 검증 미들웨어 구현
    - middlewares/auth.js : jwt을 검증하여 user 로그인 여부를 체크하는 미들웨어
    - routes/users/index.js : users 목록을 가져올 때 auth middleware 사용

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('./users.controller');
+const { authMiddleware } = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
+router.get('/', authMiddleware, userController.getAllUsers);
 router.get('/github-login', userController.githubLogin);
 router.get('/github-login/callback', userController.githubLoginCallback);
 
