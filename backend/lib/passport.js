89a402327d0b499feaa79301c6784aff1b6db5f3
kimseoyoung <caribou503@gmail.com>
2020-11-03 16:20:47 +0900

    refactor: emails 변수명 단수로 변경
    지영님 코드리뷰 내용 반영하여 emails 변수명을 email로 변경

--- backend/lib/passport.js
+++ backend/lib/passport.js
@@ -5,17 +5,17 @@ const { User } = require('../models');
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://127.0.0.1:3000/users/github-login/callback',
+  callbackURL: `${process.env.SERVER_HOST}/users/github-login/callback`,
   scope: ['user:email'],
 },
 (async (accessToken, refreshToken, profile, cb) => {
   const {
     // eslint-disable-next-line camelcase
-    id, emails: [emails], username, _json: { avatar_url },
+    id, emails: [email], username, _json: { avatar_url },
   } = profile;
   try {
     const [user] = await User.findOrCreate({
-      where: { email: emails.value },
+      where: { email: email.value },
       defaults: {
         password: id, nickname: username, account_type: 'github', profile_url: avatar_url,
       },
