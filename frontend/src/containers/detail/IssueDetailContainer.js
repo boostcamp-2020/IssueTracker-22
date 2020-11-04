fbbf638debc175b6821115f917ac6393db39ab05
BK <rlaqudrnr810@naver.com>
2020-11-04 16:57:30 +0900

    feat: feature/issue-detail-fe 브랜치 추가 & 기본 구조 설정
    feature/issue-detail-fe 브랜치 추가
    container/detail 디렉토리 생성
    container/detail/IssueDetailContainer.js 파일 생성
    container/detail/componets/CommentList.js 파일 생성 (comment list에 대한 componet)
    container/detail/componets/IssueDetail.js 파일 생성 (해당 이슈에 대한
    descript)
    container/detail/componets/Title.js 생성 (해당 이슈의 제목 관련)

---
+++ frontend/src/containers/detail/IssueDetailContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommentList from './components/CommentList';
+import IssueDetail from './components/IssueDetail';
+import Title from './components/Title';
+
+const IssueDetailContainer = () => {
+  
+  return (
+    <>
+    </>
+  );
+};
+
+export default IssueDetailContainer;
