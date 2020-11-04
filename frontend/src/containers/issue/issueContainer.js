1bc208bb42fcde146cc9ad64f5fdc5a56a7d2a20
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-11-04 15:40:29 +0900

    feat : 이슈 목록의 툴바 구현
    이슈 목록 툴바 구현(아직 이벤트리스너는 없음)

--- frontend/src/containers/issue/issueContainer.js
+++ frontend/src/containers/issue/issueContainer.js
@@ -10,13 +10,82 @@ const IssueContainer = styled.div`
     border-color: #eaecef;
 `;
 
+const dummy = {
+    "success": true,
+    "content": {
+      "issues": [
+        {
+          "id": 1,
+          "title": "bkyo",
+          "description": "prac",
+          "createdAt": "2020-11-02T15:30:00.000Z",
+          "updatedAt": "2020-11-02T15:30:00.000Z",
+          "user": {
+            "id": 1,
+            "nickname": "bk"
+          },
+          "issue_labels": [
+            {
+              "id": 1,
+              "label": {
+                "id": 1,
+                "name": "feature",
+                "color_code": null
+              }
+            },
+            {
+              "id": 3,
+              "label": {
+                "id": 1,
+                "name": "feature",
+                "color_code": null
+              }
+            }
+          ],
+          "issue_assignees": [
+            {
+              "id": 1,
+              "user": {
+                "nickname": "bk"
+              }
+            }
+          ],
+          "milestone": null
+        },
+        {
+          "id": 2,
+          "title": "bbbk",
+          "description": "bbbk",
+          "createdAt": "2020-11-03T11:11:11.000Z",
+          "updatedAt": "2020-11-03T11:11:11.000Z",
+          "user": {
+            "id": 1,
+            "nickname": "bk"
+          },
+          "issue_labels": [
+            {
+              "id": 2,
+              "label": {
+                "id": 1,
+                "name": "feature",
+                "color_code": null
+              }
+            }
+          ],
+          "issue_assignees": [],
+          "milestone": null
+        }
+      ]
+    }
+  }
 
 class Issue extends Component {
     render() {
+        // const data = getIssues()
         return (
             <IssueContainer>
-                <IssueToolbar/>
-                <IssueList/>
+                <IssueToolbar data={dummy}/>
+                <IssueList data={dummy}/>
             </IssueContainer>
         )
     }
