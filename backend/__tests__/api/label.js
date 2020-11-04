2258329f56d0d0f3f337e9ba101a18257e915092
YimJiYoung <chiy323@gmail.com>
2020-11-04 16:53:19 +0900

    test: label 조회 테스트 코드 작성
    - label 조회 응답 상태 코드, success 여부, labels 필드 포함하고 있는지 확인하는 테스트 코드 작성
    - jest 설정 추가 (--silient : console log off, --forceexit: 모든 테스트 완료 후 종료, --detectOpenHandles: 열려있는 핸들 감

--- backend/__tests__/api/label.js
+++ backend/__tests__/api/label.js
@@ -1,3 +1,12 @@
-test('api test', () => {
+const request = require('supertest');
+const app = require('../../app');
 
+describe('Label API', () => {
+  test('GET /labels', async (done) => {
+    const res = await request(app).get('/labels');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(res.body.content).toHaveProperty('labels');
+    done();
+  });
 });
