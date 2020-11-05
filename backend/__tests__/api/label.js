97b185f8db3ef57beab4f514b33e55fb39f1362a
YimJiYoung <chiy323@gmail.com>
2020-11-05 10:48:22 +0900

    test: label 테스트 코드 수정
    - beforeAll에서 request(app) 한번 하고 agent에 저장
    - req.body -> body 변수에 저장

--- backend/__tests__/api/label.js
+++ backend/__tests__/api/label.js
@@ -2,12 +2,19 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('Label API', () => {
+  let agent;
+
+  beforeAll(() => {
+    agent = request(app);
+  });
+
   test('GET /labels', async (done) => {
-    const res = await request(app).get('/labels');
+    const res = await agent.get('/labels');
+    const { body } = res;
     expect(res.statusCode).toBe(200);
-    expect(res.body.success).toBe(true);
-    expect(res.body.content).toHaveProperty('labels');
-    expect(Array.isArray(res.body.content.labels)).toBe(true);
+    expect(body.success).toBe(true);
+    expect(body.content).toHaveProperty('labels');
+    expect(Array.isArray(body.content.labels)).toBe(true);
     done();
   });
 });
