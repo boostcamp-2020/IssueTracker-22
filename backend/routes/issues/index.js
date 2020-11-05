10e870bf62696a2373e5f8b6ab549254701253f2
YimJiYoung <chiy323@gmail.com>
2020-11-05 15:02:10 +0900

    feat: issue 생성 api 구현
    - 이슈 제목, 내용, 담당자, 레이블, 마일스톤 정보를 받아 DB에 새로운 이슈를 추가하는 API 구현

--- backend/routes/issues/index.js
+++ backend/routes/issues/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { authMiddleware } = require('../../middlewares/auth');
 const issuesController = require('./issues.controller');
+
 const issue = express.Router();
 
 issue.get('/', issuesController.list);
+issue.post('/', authMiddleware, issuesController.create);
 
 module.exports = issue;
