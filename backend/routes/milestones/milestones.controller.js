ccac82efe592618817f7a2f390c691a94412717a
YimJiYoung <chiy323@gmail.com>
2020-11-05 13:46:08 +0900

    feat: milestone 추가 api 구현

--- backend/routes/milestones/milestones.controller.js
+++ backend/routes/milestones/milestones.controller.js
@@ -17,3 +17,18 @@ exports.list = async (req, res) => {
     res.status(500).json({ success: false, message: 'DB Error' });
   }
 };
+
+exports.create = async (req, res) => {
+  const { title, description, due_date } = req.body;
+  try {
+    const newMilestones = await Milestone.create({
+      title, description, due_date,
+    });
+    return res.json({
+      success: true,
+      content: { id: newMilestones.id },
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'DB Error' });
+  }
+};
