a813a7aba9843b9c0dd183a35c8f36eb039089a7
kimseoyoung <caribou503@gmail.com>
2020-11-09 22:47:59 +0900

    feat: LabelItem 컴포넌트 구현, labels 리스트 조회 기능 구현
    - src/containers/sidebar-form/components/LabelItem.js : LabelItem 컴포넌트

--- frontend/src/containers/sidebar-form/components/SelectMenuList.js
+++ frontend/src/containers/sidebar-form/components/SelectMenuList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import selectMenuMode from '../../../constants/selectMenuMode';
 import LabelItem from './LabelItem';
 import MilestoneItem from './MilestoneItem';
 import UserItem from './UserItem';
@@ -18,13 +19,13 @@ const SelectMenuList = ({ items, loading, label }) => {
 
   const { success, content } = items;
   if (!loading && success) {
-    if (label === 'Assignees') {
+    if (label === selectMenuMode.Assignees[0]) {
       const { users } = content;
       list = users.map((item) => <UserItem user={item} />);
-    } else if (label === 'Labels') {
+    } else if (label === selectMenuMode.Labels[0]) {
       const { labels } = content;
-    //   list = labels.map((item) => <LabelItem label={item} />);
-    } else if (label === 'Milestone') {
+      list = labels.map((item) => <LabelItem label={item} />);
+    } else if (label === selectMenuMode.Milestone[0]) {
       const { milestones } = content;
     //   list = milestones.map((item) => <MilestoneItem milestone={item} />);
     }
