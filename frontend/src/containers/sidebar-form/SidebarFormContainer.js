dcdbd7cde15fc3751a715c2049c9967ef1ebe9c8
kimseoyoung <caribou503@gmail.com>
2020-11-09 22:32:28 +0900

    feat: sidebar form 컨테이너 구현
    - 이슈 작성 화면의 우측 SidebarForm 컨테이너 구현
    - src/constants/selectMenuMode.js : SidebarFormContainer를 Assignees, Labels, Milestone 세가지 경우로 구분해 사용하기 위한 상수 선언
    - src/containers/sidebar-form/SidebarFormContainer.js : sidebar form을 담는 컨테이너

---
+++ frontend/src/containers/sidebar-form/SidebarFormContainer.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import SelectMenuContainer from './SelectMenuContainer';
+
+const SidebarHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 0;
+  position: relative;
+  :hover {
+    color: skyblue;
+  }
+`;
+
+const SidebarTitle = styled.span`
+  font-weight: 600;
+`;
+
+const GearButton = styled.button`
+  all: unset; 
+`;
+
+const SelectedItemList = styled.div`
+`;
+
+const SelectedItem = styled.div`
+`;
+
+const SidebarFormContainer = ({ mode }) => {
+  const [label, statusText, selectMenuHeader, url] = mode;
+  const [open, setOpen] = useState(false);
+
+  const showSelectMenu = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div style={{ padding: '10px 0', borderBottom: '1px solid lightgray' }}>
+      <input type="hidden" value="sidebar" name={label}/>
+
+      <SidebarHeader>
+        <SidebarTitle>{label}</SidebarTitle>
+        <GearButton type="button" onClick={showSelectMenu}>@</GearButton>
+        {/* <SelectMenuContainer open={open} url={url} label={label} header={selectMenuHeader} /> */}
+      </SidebarHeader>
+
+      <SelectedItemList>
+        <div>{statusText}</div>
+        {/* <SelectedItem /> */}
+      </SelectedItemList>
+
+    </div>
+  );
+};
+
+export default SidebarFormContainer;
