8b3b5ba3e735468e853d12d6ce305ae8e0396542
YimJiYoung <chiy323@gmail.com>
2020-11-09 11:35:05 +0900

    feat: Header 스타일 적용

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import GitHubIcon from '../assets/icon/GitHubIcon';
+
+const HeaderWrapper = styled.header`
+  color: white;
+  background-color: #24292e;
+  height: 80px;
+  text-align: center;
+  padding: 10px;
+`;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+`;
+
+const IconWrapper = styled.div`
+  margin: 5px;
+`;
 
 const Header = () => (
-  <header>ISSUES</header>
+  <HeaderWrapper>
+    <Title>
+      <IconWrapper>
+        <GitHubIcon/>
+      </IconWrapper>
+      <h2>ISSUES</h2>
+    </Title>
+  </HeaderWrapper>
 );
 
 export default Header;
