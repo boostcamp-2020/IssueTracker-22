0bb463e74c77f94d1fa70c0c446b33f965b90706
BK <rlaqudrnr810@naver.com>
2020-11-10 19:19:04 +0900

    feat: detail data 출력
    dummy data가 아닌 실제 데이터 출력

--- frontend/src/containers/detail/IssueDetailContainer.js
+++ frontend/src/containers/detail/IssueDetailContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect }from 'react';
 import Header from '../../components/Header';
 import Title from './components/Title';
 import TitleBox from './components/TitleBox';
@@ -9,100 +9,54 @@ import List from './components/List';
 import IssueDetail from './components/IssueDetail';
 import CommentList from './components/CommentList';
 import Side from './components/Side';
+import CreateComment from './components/CreateComment';
+import apiUri from '../../constants/api';
 
-const dummy = {
-  success: true,
-  issues: [
-    {
-      id: 1,
-      title: 'bkyo',
-      description: 'prac',
-      is_open: 1,
-      createdAt: '2020-11-02T15:30:00.000Z',
-      updatedAt: '2020-11-02T15:30:00.000Z',
-      user: {
-        id: 1,
-        nickname: 'bk',
-        profile_url: 'https://avatars2.githubusercontent.com/u/37496919?v=4',
-      },
-      comments: [
-        {
-          id: 1,
-          description: 'For test',
-          author_id: 3,
-          createdAt: '2020-10-02T15:30:00.000Z',
-          updatedAt: '2020-10-04T15:30:00.000Z',
-          user: {
-            id: 3,
-            nickname: 'YimJiYoung',
-            profile_url: 'https://avatars2.githubusercontent.com/u/37496919?v=4',
-          },
-        },
-        {
-          id: 2,
-          description: 'For test2',
-          author_id: 3,
-          createdAt: '2020-10-02T15:30:00.000Z',
-          updatedAt: '2020-10-04T15:30:00.000Z',
-          user: {
-            id: 3,
-            nickname: 'BKBKBK',
-            profile_url: 'https://avatars2.githubusercontent.com/u/37496919?v=4',
-          },
-        },
-      ],
-      milestone: {
-        id: 1,
-        title: 'week1',
-      },
-      issue_labels: [
-        {
-          id: 1,
-          label: {
-            id: 1,
-            name: 'feature',
-            color_code: '#7DCDA0',
-          },
-        },
-        {
-          id: 3,
-          label: {
-            id: 1,
-            name: 'feature',
-            color_code: '#7DCDA0',
-          },
-        },
-      ],
-      issue_assignees: [
-        {
-          id: 1,
-          user: {
-            id: 1,
-            nickname: 'bk',
-            profile_url: null,
-          },
-        },
-      ],
-    },
-  ],
+const getData = () => {
+  const url = apiUri.detail + document.location.href.split("/")[5];
+  const option = {
+    mode: 'cors',
+    credentials: 'include',
+    method: 'GET',
+  }
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  async function fetchUrl() {
+    const response = await fetch(url, option);
+    const json = await response.json();
+    setData(json);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [data, loading];
+}
+
+
+const IssueDetailContainer = () => {
+  const [data, loading] = getData();
+  if(!loading) {
+    const issue = data.content.issues[0];
+    return <>
+      <Header />
+      <TitleBox>
+        <Title>{ issue }</Title>
+        <TitleEditButton />
+      </TitleBox>
+      <TitleDetail>{ issue }</TitleDetail>
+      <IssueContent>
+        <List>
+          <IssueDetail>{ issue }</IssueDetail>
+          {CommentList(issue.comments)}
+          <CreateComment data={issue}/>
+        </List>
+        <Side />
+      </IssueContent>
+    </>;
+  }
+  return <>loading...</>;
 };
 
-const IssueDetailContainer = () => (
-  <>
-    <Header />
-    <TitleBox>
-      <Title>{ dummy.issues[0] }</Title>
-      <TitleEditButton />
-    </TitleBox>
-    <TitleDetail>{ dummy.issues[0] }</TitleDetail>
-    <IssueContent>
-      <List>
-        <IssueDetail>{ dummy.issues[0] }</IssueDetail>
-        {CommentList(dummy.issues[0].comments)}
-      </List>
-      <Side />
-    </IssueContent>
-  </>
-);
 
 export default IssueDetailContainer;
