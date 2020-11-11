a25dabaf11ff4e2bf1acbfc208e36e1ed14ad66d
DESKTOP-T1A999T\miste <mistercle@gmail.com>
2020-11-12 03:53:59 +0900

    feat : 닫힌 이슈 빨간 svg로 교체
    이슈가 닫힌 상태일때 빨간 그림이 제목 앞에 오도록 변경

--- frontend/src/containers/issue/components/IssueList.js
+++ frontend/src/containers/issue/components/IssueList.js
@@ -4,20 +4,33 @@ import renderIssueLowerBox from './IssueLowerBox';
 import renderIssueUpperBox from './IssueUpperBox';
 import IssueAssignee from './IssueAssignee';
 // import IssueMilestone from './issueMilestone';
-import { svgOpen } from '../../../assets/svgPath';
+import { svgOpen, svgClose } from '../../../assets/svgPath';
 
 class IssueInfo extends Component {
+
   render() {
     const { id } = this.props.issue;
+    const drawSVG = (issue) => {
+      console.log(issue.is_open)
+      if(issue.is_open > 0) {
+        return (
+          <svg style={{ fill: '#22863A' }} viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+            {svgOpen}
+          </svg>
+        )
+      } else {
+        return (<svg style={{ fill: 'red' }} viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+            {svgClose}
+        </svg>)
+      }
+    }
     return (
       <IssueBox id={id}>
         <div style={{ padding: '8px 0px 8px 16px' }}>
           <input type="checkbox" name="issue-checkbox"/>
         </div>
         <div style={{ padding: '8px 0px 0px 16px' }}>
-          <svg style={{ fill: '#22863A' }} viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-            {svgOpen}
-          </svg>
+          {drawSVG(this.props.issue)}
         </div>
         <div className="Issue-ContentBox" style={{ padding: '8px' }}>
           {renderIssueUpperBox(this.props)}
