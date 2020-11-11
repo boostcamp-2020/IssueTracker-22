785af30d91e6817701e91d839502396f367ae8d2
YimJiYoung <chiy323@gmail.com>
2020-11-11 14:07:36 +0900

    feat: Label 페이지 컴포넌트 생성, Route에 추가
    - Label pages에 컴포넌트 생성
    - Route에 path, component 추가

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import pathUri from './constants/path';
 import {
-  Login, Issue, CreateIssue, IssueDetail,
+  Login, Issue, CreateIssue, IssueDetail, Label,
 } from './pages';
 
 const routes = () => (
@@ -11,6 +11,7 @@ const routes = () => (
     <Route exact path={pathUri.createIssue} component={CreateIssue}/>
     <Route path={pathUri.issueDetail} component={IssueDetail}/>
     <Route path={pathUri.issue} component={Issue}/>
+    <Route path={pathUri.label} component={Label}/>
   </Switch>
 );
 
