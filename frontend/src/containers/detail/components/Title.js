4c95a1b0c706446046c3ab8d35af50c3b358d486
BK <rlaqudrnr810@naver.com>
2020-11-12 15:55:40 +0900

    feat: edit, save, cancel 버튼 구현
    title edit, save, cancel 버튼 구현 및 동작
    db에 적용 구현 예정
    UI 보완 예정

--- frontend/src/containers/detail/components/Title.js
+++ frontend/src/containers/detail/components/Title.js
@@ -23,7 +23,7 @@ const IdStyle = styled.div`
   margin-left: 10px;
   font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
 `;
-const Button = styled.button`
+const EditButton = styled.button`
   float: left;
   margin: 0;
   padding: 3px 12px;
@@ -39,17 +39,82 @@ const Button = styled.button`
   width: 70px
 `;
 
-const Title = (data) => {
+const SaveButton = styled.button`
+  float: left;
+  margin: 0;
+  padding: 3px 12px;
+  font-size: 16px;
+  line-height: 20px;
+  color: #fff;
+  background-color: #2ea44f;
+  border: 1px solid;
+  border-radius: 6px;
+  cursor: pointer;
+  margin-left: auto;
+  height: 40px;
+  width: 70px
+`;
 
+const CancelButton = styled.button`
+  float: left;
+  margin: 0;
+  padding: 3px 12px;
+  font-size: 16px;
+  line-height: 20px;
+  color: #fff;
+  background-color: #2ea44f;
+  border: 1px solid;
+  border-radius: 6px;
+  cursor: pointer;
+  margin-left: auto;
+  height: 40px;
+  width: 70px
+`;
+
+
+const Title = (data) => {
+  const { children } = data;
+  const [title, setTitle] = useState(children.title);
+  const [mode, setMode] = useState(false);
+  const [basicTitle, setBasicTitle] = useState(children.title);
+  const changeMode = () => {
+    setMode(!mode);
+  }
+  const saveEditTitle = () => {
+    //fetch로 update...
+    setBasicTitle(title);
+    setMode(!mode);
+  }
+  const cancelEditTitle = () => {
+    setMode(!mode);
+    setTitle(basicTitle);
+  }
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+  if(mode){
+    return (
+      <>
+      <TitleBox>
+        <input value={title} onChange={onChange} />
+        <CancelButton onClick={ cancelEditTitle }>cancel</CancelButton>
+        <SaveButton onClick={ saveEditTitle }>save</SaveButton>
+      </TitleBox>
+      </>
+    );
+  }
+
+  
   return (
     <>
     <TitleBox>
       <TitleStyle>{ title }</TitleStyle>
       <IdStyle>#{ children.id }</IdStyle>
-      <Button>Edit</Button>
+      <EditButton onClick={ changeMode }>Edit</EditButton>
     </TitleBox>
     </>
   );
+
 };
 
 export default Title;
