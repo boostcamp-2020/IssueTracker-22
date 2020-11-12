31d367a77fe0ee53cdf4201de3dfa6e06ea343c9
BK <rlaqudrnr810@naver.com>
2020-11-12 17:43:49 +0900

    feat: 이슈 상세 description 수정 기능 구현
    이슈 상세 description 수정 기능 구현

--- frontend/src/containers/detail/components/IssueDetail.js
+++ frontend/src/containers/detail/components/IssueDetail.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import RelativeTime from '../../../lib/relativeTime';
 import UserProfileContainer from '../../user-profile/UserProfileContainer';
+import CommentEditor from '../../issue-form/components/CommentEditor';
+import imageUploadHandler from '../../../lib/imageUploadHandler';
+import apiUri from '../../../constants/api';
+
+const Input = styled.div`
+  width: 100%;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: white;
+`;
 
 const IssueDetailContainer = styled.div`
   width: 100%;
@@ -78,28 +89,132 @@ const EditButton = styled.button`
   width: auto;
   outline:none;
 `;
+const CancelButton = styled.button`
+  all: unset;
+  display: block;
+  font-size: 15px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: white;
+  font: 400 13.3333px Arial;;
+  font-size: 15px;
+  padding: 8px 15px;
+  margin-left:auto;
+  margin-right: 10px;
+`;
+const SaveButton = styled.button`
+  display: block;
+  color: white;
+  font-size: 15px;
+  background-color: #0DBF18;
+  border: 1px solid #009B09;
+  border-radius: 5px;
+  padding: 8px 15px;
+  cursor: pointer;
+`;
+
+const FlexRowBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  flex-wrap: wrap;
+`;
+
 const IssueDetail = (data) => {
   const { children } = data;
-  const time = RelativeTime(children.updatedAt);
+  const time = RelativeTime(children.createdAt);
   const { user } = children;
+  const [description, setDescription] = useState(children.description);
+  const [mode, setMode] = useState(false);
+  const [basicDesc, setBasicDesc] = useState(children.description);
+  const renderImageTag = async (file) => {
+    const { name: imageAlt } = file;
+      
+    try {
+      const imageUrl = await imageUploadHandler(file);
+      const imageTag = `\n\n<img alt="${imageAlt}" src="${imageUrl}">\n\n`;
+      
+      const newDescription = description + imageTag;
+      setDescription(newDescription);
+      
+    } catch (error) {
+      alert('failed to upload image');
+    }
+  };
+  const uploadFile = (e) => {
+    const { files } = e.target;
+    files.forEach((file) => renderImageTag(file));
+  };
+  const changeMode = () => {
+    setMode(!mode);
+  }
+  const cancelEditDesc = () => {
+    setMode(!mode);
+    setDescription(basicDesc);
+  }
+  const saveEditDesc = async () => {
+    //fetch -> description update...
+    const body = {
+      issue_id: children.id,
+      description: description
+    }
 
-  return (
-    <>
+    const response = await fetch(apiUri.issueUpdate, {
+      mode: 'cors',
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const { success, message } = await response.json();
+
+    if (!success) {
+      alert(message);
+      return;
+    }
+
+
+    setBasicDesc(description);
+    setMode(!mode);
+
+  }
+
+  const setIssueDesc = (e) => {
+    setDescription(e.target.value);
+  };
+
+  if(mode) {
+    return <>
+      <IssueDetailContainer>
+        <UserProfileContainer user={user} />
+          <Input>
+            <CommentEditor onChange={setIssueDesc} value={description} onFileUpload={uploadFile}/>
+            <FlexRowBetween>
+              <CancelButton onClick={cancelEditDesc}>cancel</CancelButton>
+              <SaveButton type="submit" value="Submit" onClick={saveEditDesc}>save</SaveButton>
+            </FlexRowBetween>
+          </Input>
+      </IssueDetailContainer>
+    </>;
+  }
+  return <>
       <IssueDetailContainer>
         <UserProfileContainer user={user} />
         <IssueDetailStyle>
           <CommentTitle>
             <Author>{ children.user.nickname}</Author>
             <Info>commented { time }</Info>
-            <EditButton>Edit</EditButton>
+            <EditButton onClick={changeMode}>Edit</EditButton>
           </CommentTitle>
           <CommentDescription>
-            <Description>{ children.description }</Description>
+            <Description>{ description }</Description>
           </CommentDescription>
         </IssueDetailStyle>
       </IssueDetailContainer>
-    </>
-  );
+    </>;
 };
 
 export default IssueDetail;
